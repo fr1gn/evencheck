3603f879c4128e5161cdbc04bf72d4afa3b5230b
Tairlan <231151@astanait.edu.kz>
2023-12-13 09:34:54 +0300

    Check numbers for even

---
+++ check.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class check {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if(num%2==0) {
+            System.out.println("Четное число");
+        } else {
+            System.out.println("Нечетное число");
+        }
+    }
+}
